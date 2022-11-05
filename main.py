c2b250ce5e637cfc97952f5f2dd3674eb1aedaa5
User <sorus10q@yandex.ru>
2022-11-05 10:08:14 +0300

    Добавил суперфункцию

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+def my_supersunction():
+    print("Много принтов")
+
 def main():
     print("Первый принт")
     print("Новый день новый принт")
     print("Еще принт")
     print("Сижу в поезде и много думаю")
     print("Все еще в поезде")
-    
+    my_supersunction()
 if __name__ == '__main__':
     main()
 
